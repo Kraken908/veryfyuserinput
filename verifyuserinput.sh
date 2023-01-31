d97d6d03a930f9eb1da5738bc741185dd0b08b88
kraken <70107358+Kraken908@users.noreply.github.com>
2023-01-31 16:52:46 -0700

    Create verifyuserinput.sh

---
+++ verifyuserinput.sh
@@ -0,0 +1,4 @@
+read -p "Enter your ZIP code: " zip                                                          
+if [ $zip -eq 99999 ]                                   
+then
+echo "You are in the Mars zip code"                   
