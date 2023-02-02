6533c5750d3c3da9e063a1ac6109af4c834da5f2
kraken <70107358+Kraken908@users.noreply.github.com>
2023-02-02 14:56:04 -0700

    Update verifyuserinput.sh

--- verifyuserinput.sh
+++ verifyuserinput.sh
@@ -2,3 +2,5 @@ read -p "Enter your ZIP code: " zip
 if [ $zip -eq 99999 ]                                   
 then
 echo "You are in the Mars zip code"                   
+
+#the richard toxic guy face verifyuser input
